4e2695c328ecc58de6f53e5c0f83861ab1abea3e
abelsan <abel@mit.edu>
2017-01-03 00:35:18 -0500

    fireone redirect

--- fireone.js
+++ fireone.js
@@ -8,75 +8,39 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
 function fireone(){
-  if(localStorage.getItem('fireone')){
-    console.log('fireone found!');    
-    var fireone = localStorage.getItem('fireone');
-    fireone = JSON.parse(fireone);
-    console.log(fireone);
 
-    // verify token    
-    try{
-      var url = 'https://api.github.com/user?access_token=' + fireone.accessToken;
-      var xhr = new XMLHttpRequest();
-      xhr.onload = function(){ verify(xhr,fireone); };
-      xhr.open("GET",url);
-      xhr.send();    
-    }    
-    catch(e){
-      console.log('Verification failed: ' + e);
-      authenticate();      
-    }
-  }
-  else{
-    console.log('fireone NOT found!');        
-    authenticate();
-  }
-}
-
-function verify(xhr, fireone) {
-  var response = xhr.response;
-  if (response){
-    response = JSON.parse(response);
-    if (fireone.login === response.login){ 
-      return; 
+    // fireone token check
+    if(localStorage.getItem('fireone')){
+        console.log(JSON.parse(localStorage.getItem('fireone')));
+        return;        
     }
     else{
-      console.log('Verification failed, logins different!');
-      authenticate();            
+        firebase.auth().getRedirectResult().then(function(result){
+            var fireone = {};
+            if(!result.credential){
+                // user not found, authenticate user
+                var provider = new firebase.auth.GithubAuthProvider();
+                firebase.auth().signInWithRedirect(provider);
+                return;
+            }
+
+            // user authenticated, access properties
+            fireone.email = result.user.email;
+            console.log(fireone.email);
+
+            // use accessToken for github api
+            fireone.token = result.credential.accessToken;
+            var url = 'https://api.github.com/user?access_token=' + fireone.token;
+            var xhr = new XMLHttpRequest();
+            xhr.onload = function(){ handler(xhr,fireone); };
+            xhr.open("GET",url);
+            xhr.send();
+
+        }).catch(function(error){
+            console.log('Failed Login to Firebase', error);        
+        });    
     }
-  }
-  else{
-    console.log('Verification failed, reponse empty!');
-    authenticate();          
-  }
-}
-
-function authenticate(){
-  var fireone = {};
-  var provider = new firebase.auth.GithubAuthProvider();
-
-  // pop up authentication
-  // -------------------------    
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // GitHub Access Token
-    var token = result.credential.accessToken;
-    fireone.accessToken = token;
-    var user = result.user;
-    fireone.email = user.email;    
-
-    var url = 'https://api.github.com/user?access_token=' + token;
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function(){ handler(xhr,fireone); };
-    xhr.open("GET",url);
-    xhr.send();
-  }).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-  });
 
 }
 
