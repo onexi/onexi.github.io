61912b5a2da6fdfd568987e2204d20b4b02dc8db
jrw0mitedu <jrwtango@gmail.com>
2017-02-21 06:18:02 -0500

    L4 Rev4

---
+++ jrw/L4 JSON/starter_code/contacts.js
@@ -0,0 +1,35 @@
+var data = {
+    contacts: [{
+            name: 'john',
+            email: 'jrw@mit.edu',
+            courses: [
+                '1.00', '1.125'
+            ],
+            hobbies: ['golf', 'tango']
+        },
+        {
+            name: 'abel',
+            email: 'abel@mit.edu',
+            courses: [
+                '1.00', '1.125'
+            ],
+            hobbies: ['basketball', 'guitar']
+        },
+        {
+            name: 'nadia',
+            email: 'nadia@mit.edu',
+            courses: [
+                '1.00', '6.001'
+            ],
+            hobbies: ['french', 'skydiving']
+        },
+        {
+            name: 'isabella',
+            email: 'isabella@mit.edu',
+            courses: [
+                '1.00', '15.131'
+            ],
+            hobbies: ['swimming', 'travel']
+        }
+    ]
+};
