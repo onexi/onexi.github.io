2c529bacd134771f7d086b336bfca741b3d5d102
abelsan <abel@mit.edu>
2017-05-14 22:50:29 -0400

    project videos

---
+++ assets/js/videos.js
@@ -0,0 +1,410 @@
+var videos = [];
+
+videos.push({
+	'team' 	 : 'Angel Eye',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'AmiK0GcwpAs'
+		},
+		{
+			'deliverable' : '02',
+			'link' : 'JoGPoOCKl-s'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'ErvFQ9jiUHw'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'ew-jqFpqB9Q'
+		}
+	]
+});  
+
+videos.push({
+	'team' 	 : 'The Lone Rider',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'gLsWfUFTwz4'
+		},
+		{
+			'deliverable' : '02',
+			'link' : 'Qjqk9_1u93o'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'WpRWpHaHy8c'
+		},
+		{
+			'deliverable' : '04',
+			'link' : '5cR64X1tHZo'
+		}
+	]
+});  
+
+videos.push({
+	'team' 	 : 'FlightSnap',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'jU6Qy-J3fvQ'
+		},
+		{
+			'deliverable' : '02',
+			'link' : 'uCRukHwIoug'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'lfNln2iUEa4'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'lCol_IH1iiw'
+		}
+	]
+});  
+
+
+videos.push({
+	'team' 	 : 'DrinkMe',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'gy2zVg45Bxk'
+		},
+		{
+			'deliverable' : '02',
+			'link' : '5iLoDgxee8M'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'THDVJSOVIDs'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'Qbfzi9Fvf8c'
+		}
+	]
+});  
+
+videos.push({
+	'team' 	 : 'House Matters',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'GAnUONC6Udk'
+		},
+		{
+			'deliverable' : '02',
+			'link' : 'EcrXkJLo5nY'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'lLmzSfMG2K0'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'D-GzZl883mM'
+		}
+	]
+}); 
+
+videos.push({
+	'team' 	 : 'Work Interest',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'HwZuQZsuUsA'
+		},
+		{
+			'deliverable' : '02',
+			'link' : 'Ych9AZA2WcI'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'SCP5FBSbrbc'
+		},
+		{
+			'deliverable' : '04',
+			'link' : '0Yl4nI5Rqac'
+		}
+	]
+});   
+
+videos.push({
+	'team' 	 : 'Golden Lyrics',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : '2bgZyDNENtc'
+		},
+		{
+			'deliverable' : '02',
+			'link' : '2CNSRoA2-2E'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'n4S7vJ6LLGY'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'tpUzZRoKAHE'
+		}
+	]
+});  
+
+videos.push({
+	'team' 	 : 'Tieng Xie',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'tHb2du1zUqo'
+		},
+		{
+			'deliverable' : '02',
+			'link' : '-jLDvBRkLCY'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'oa3z-Tb9i_Y'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'QK5xRLuBPjQ'
+		}
+	]
+});  
+  
+
+  
+videos.push({
+	'team' 	 : 'Weather Busters',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'AfrQ1mNrC2k'
+		},
+		{
+			'deliverable' : '02',
+			'link' : 'a3LpOrFNcns'
+		},
+		{
+			'deliverable' : '03',
+			'link' : '6O_4FfQHtyw'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'd6j2TsRNalA'
+		}
+	]
+}); 
+
+videos.push({
+	'team' 	 : 'Capital Tweets',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : '0nN2LQP76qU'
+		},
+		{
+			'deliverable' : '02',
+			'link' : 'lmUIp2zpOog'
+		},
+		{
+			'deliverable' : '03',
+			'link' : '3IFLmr-JSVI'
+		}
+	]
+});    
+
+
+videos.push({
+	'team' 	 : 'Volta',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'TWi2ShhTxck'
+		},
+		{
+			'deliverable' : '02',
+			'link' : '6aSPR14TxHc'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'Y-YZOah_euI'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'MNCPVT9nNvA'
+		}
+	]
+});  
+  
+
+videos.push({
+	'team' 	 : 'LGO Edge Fund',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'Hqx1jxNvz4Q'
+		},
+		{
+			'deliverable' : '02',
+			'link' : 'qf8LKmoB3Xc'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'CWLHZNXIoO8'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'Wxr4DwjnFOc'
+		}
+	]
+});  
+  
+
+videos.push({
+	'team' 	 : 'Emotional State of A City',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'd3kZBTxGYak'
+		},
+		{
+			'deliverable' : '02',
+			'link' : '3bN12-QKhzc'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'BKdj4EB1lKY'
+		}
+	]
+});  
+  
+
+  
+videos.push({
+	'team' 	 : 'OverScheduled',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'Oav9ZYT_XNQ'
+		},
+		{
+			'deliverable' : '02',
+			'link' : 'Z3wBHj1ml8c'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'Dr2aFCEsroo'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'sdiwk9-rN3E'
+		}
+	]
+});   
+
+videos.push({
+	'team' 	 : 'Analyze Boston',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : '5yCXhb1Rb30'
+		},
+		{
+			'deliverable' : '02',
+			'link' : 'm2YarOR63Jw'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'I2oOlFB59as'
+		}
+	]
+});
+  
+
+videos.push({
+	'team' 	 : 'Mob Feed',
+	'videos' : [
+		{
+			'deliverable' : '01',
+			'link' : 'yE8qHpKvRoo'
+		},
+		{
+			'deliverable' : '02',
+			'link' : '01h1s_01ev0'
+		},
+		{
+			'deliverable' : '03',
+			'link' : 'xcfNmdbvX6Y'
+		},
+		{
+			'deliverable' : '04',
+			'link' : 'DfFi4c7tSe8'
+		}
+	]
+}); 
+  
+
+  
+
+  
+
+
+function loadVideos(){
+	var html = '';
+	videos.forEach(function(project){
+
+      	html += '<div class="well">';
+        html += '<div class="row">';
+
+		project.videos.forEach(function(video){
+
+        	html += '<div class="col-md-3">';
+            html += '    <div class="youtube-player" data-id="' + video.link + '"></div>';
+          	html += '</div>';
+
+		});
+
+        html += '</div>';
+        html += '</div>';                
+	});
+	document.getElementById('target').innerHTML = html;
+}
+loadVideos();
+
+
+/* Light YouTube Embeds by @labnol */
+/* Web: http://labnol.org/?p=27941 */
+
+document.addEventListener("DOMContentLoaded",
+    function() {
+        var div, n,
+            v = document.getElementsByClassName("youtube-player");
+        for (n = 0; n < v.length; n++) {
+            div = document.createElement("div");
+            div.setAttribute("data-id", v[n].dataset.id);
+            div.innerHTML = labnolThumb(v[n].dataset.id);
+            div.onclick = labnolIframe;
+            v[n].appendChild(div);
+        }
+    });
+
+function labnolThumb(id) {
+    var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
+        play = '<div class="play"></div>';
+    return thumb.replace("ID", id) + play;
+}
+
+function labnolIframe() {
+    var iframe = document.createElement("iframe");
+    var embed = "https://www.youtube.com/embed/ID?autoplay=1";
+    iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
+    iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("allowfullscreen", "1");
+    this.parentNode.replaceChild(iframe, this);
+}
