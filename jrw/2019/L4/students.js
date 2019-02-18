eb4ebadbfc4bd2c5e5d0297ab1956b19f01d8ea3
John Williams <jrw@mit.edu>
2019-02-18 12:04:34 -0500

    L4a

---
+++ jrw/2019/L4/students.js
@@ -0,0 +1,196 @@
+module.exports.myFunc = function(){
+
+return  [{'name':'john williams', 
+	'email':'jrw@mit.edu',
+	'department': '1 - CEE',
+	'year':null,
+	'computing-experience':20,
+	'role':'lecturer'
+},
+{'name':'abel sanchez', 
+	'email':'abel@mit.edu',
+	'department': '1 - CEE',
+	'year':null,
+	'computing-experience':'20',
+	'Role':'lecturer'
+
+},
+{'name':'luciana telles',
+	'email':'telleslu@gmail.com',
+	'department': 'visitor',
+	'year':2019,
+	'Computing-experience':36,
+	'Role':'researcher'
+},
+
+{'name': 'Stephanie Baez',
+	'email': 'smbaez@mit.edu',
+	'department': '1-CEE',
+	'year':'2022',
+	'Computing-experience':0,
+	'Role': 'student'
+},
+
+{'name':'Shiqing Liu',
+'email':'shiqing@mit.edu',
+'department':'1-CEE',
+'Year':'2022',
+'Computing-experience':10,
+'Role':'Student'
+},
+
+{'name': 'Bensu Manav',
+'email': 'bensu@mit.edu',
+'department': '1 - CEE',
+'year': 'grad',
+'computing-experience': 3,
+'role': 'student'
+},
+
+{'name': 'Siddharth Sridhar',
+'email': 'ss2k@mit.edu',
+'department': '1 - CEE',
+'year': 2022,
+'computing-experience': 12,
+'role': 'student'
+},
+{'name':'oluwanifemi ajayi',
+'email':'najayi@mit.edu',
+'department':'1-CEE',
+'year':'2019',
+'computing-experience':24,
+'role':'student'
+},
+{'name':'Neosha Narayanan',
+	'email': 'neosha@mit.edu',
+	'department': '1 (CEE) or 3 (MSE)',
+	'year': 2022,
+	'computing-experience': 48,
+	'role': 'student'
+},
+
+{'name': 'Jessica Arbuckle',
+	'email': 'rbucklej@mit.edu',
+	'department': 'thinking 1 or 3',
+	'year': '2022',
+	'computing-experience': 1,
+	'role': 'student'
+},
+
+
+{'name': 'Anna Landler', 
+	'email': 'alandler@mit.edu',
+	'department': '1 - CEE',
+	'year': '2022',
+	'computing-experience':1,
+	'role':'student'
+},
+
+
+{'name':'Zilong He', 
+	'email':'zilonghel@mit.edu',
+	'department': '2 - ME',
+	'year':2019,
+	'computing-experience':1,
+	'Role':'student'
+
+},
+
+{'name':'Yu Qiu',
+ 'email': 'yuqiu@mit.edu',
+ 'Department': '1-cee',
+ 'year': 2019,
+ 'Computing experience': 0,
+ 'Role': 'student'
+},
+
+{'name': 'Xiyang Yuan',
+ 'email': 'xyyuan@mit.edu',
+ 'Department': 'SCM',
+ 'Year': 2019,
+ 'Computing-experience': 3,
+ 'Role': 'student'
+},
+{'name': 'Kelly Craig',
+	'email': 'kcraig1@mit.edu',
+	'Department': '15-3 - Finance',
+	'Year': 'junior',
+	'Computing-experience': 0,
+	'Role': 'student'
+},
+
+{'Name':'Dipo Doherty',
+	'email':'dohertyo@mit.edu',
+	'Department':'IDM',
+	'Year':'2020',
+	'Computing experience':'6',
+	'Role':'Student',
+},
+
+{'name':'Claire Yost', 
+	'email':'cyost@mit.edu',
+	'department': '1 - CEE',
+	'year': 2021,
+	'computing-experience':0,
+	'role':'student'
+
+},
+
+{'name':'kenta yoshida', 
+	'email':'kenta_y@mit.edu',
+	'department': '15-Management',
+	'year':2019,
+	'computing-experience':0,
+	'role':'student'
+},
+
+
+{'name':'patricia arnal', 
+	'email':'arnaluna@mit.edu',
+	'department': '1 - SDM',
+	'year':1,
+	'computing-experience':0,
+	'Role':'student'
+},
+
+{'name': 'Yafang Yang',
+	'email': 'yafang@mit.edu',
+	'department': '8-Physics',
+	'year': '2012',
+	'computing-experience': 3,
+	'Role': 'student'
+},
+
+
+{'name':'Keran Rong', 
+	'email':'krong@mit.edu',
+	'department': '1 - SDM',
+	'year':'2020',
+	'computing-experience':5,
+	'role':'student'
+},
+
+{'name':'Arjun Chandar', 
+	'email':'achandar@mit.edu',
+	'department': '2 - Mechanical Engineering',
+	'year':'PhD Year 1',
+	'computing-experience':3,
+	'role':'listener'
+},
+{'name':'Frankie Pineda', 
+'email':'fpineda@mit.edu',
+'department':'2',
+'year':'junior',
+'computing-experience':'2',
+'role':'student'
+},
+
+{'name':'Jared Prince', 
+	'email':'jkprince@mit.edu',
+	'department': '2 - ME',
+	'year':'2022',
+	'computing-experience':6,
+	'Role':'student'
+}
+];
+};
